9ac5eb1827264c372956af1e711a961a5315cacf
Guilherme Raibolt Efgen <guiefgen@gmail.com>
2024-06-24 11:02:12 -0300

    TL finalizado

--- TL_gre.asm
+++ TL_gre.asm
@@ -16,9 +16,27 @@ segment code
     MOV     WORD [ES:intr*4], relogio ; Define o offset para a nova rotina de interrupção
     STI                    ; Habilita interrupções
 
+; salvar modo corrente de video(vendo como est� o modo de video da maquina)
+        mov  		ah,0Fh
+        int  		10h
+        mov  		[modo_anterior],al   
+
+		
+
+
+; alterar modo de video para gr�fico 640x480 16 cores
+    	mov     	al,12h
+   		mov     	ah,0
+    	int     	10h
+		
+
+;Inicialização programa cores default
+	mov byte[cor],branco_intenso 
+
 l1:	
     cmp     byte [tique], 0 ; Compara o valor de `tique` com 0
     jne     ab             ; Se `tique` não for 0, pula para ab
+    call escreve_menu
     call    converte       ; Chama a função `converte` para atualizar a exibição
 ab:
     mov     ah, 0bh        ; Função 0Bh de interrupção 21h: Verifica o buffer de teclado
@@ -32,7 +50,7 @@ tratativa_teclado:
     int 21h
 
     cmp al, 'q'
-    je fim
+    je near fim
     cmp al, 's'
     je opcao_s
     cmp al, 'm'
@@ -42,54 +60,75 @@ tratativa_teclado:
     jmp l1
 
 opcao_s:
-    mov ah, 7
-    int 21h
+    call checa_ascii
+
     sub al, '0'
     mov bl, 10
 	mul bl
     mov dl, al
 
-    mov ah, 7
-    int 21h
+    call checa_ascii
+
     sub al, '0'
     add dl, al
+
+
+    cmp dl, 59
+    jg near deu_erro
 
     mov byte[segundo], dl
-    call converte
-    jmp l1
+
+    jmp apaga_erro
+
+
 opcao_m:
-    mov ah, 7
-    int 21h
+    
+    call checa_ascii
+
     sub al, '0'
     mov bl, 10
 	mul bl
     mov dl, al
 
-    mov ah, 7
-    int 21h
+    call checa_ascii
     sub al, '0'
     add dl, al
+    
+    cmp dl, 59
+    jg near deu_erro
+
 
     mov byte[minuto], dl
-    call converte
-    jmp l1
+
+    jmp apaga_erro
+
 opcao_h:
-    mov ah, 7
-    int 21h
+    call checa_ascii
     sub al, '0'
     mov bl, 10
 	mul bl
     mov dl, al
 
-    mov ah, 7
-    int 21h
+    call checa_ascii
     sub al, '0'
     add dl, al
 
+    cmp dl, 23
+    jg near deu_erro
     
-    mov byte[hora], dl
-    call converte
-    jmp l1
+        mov byte[hora], dl
+
+    jmp apaga_erro
+
+checa_ascii:
+    mov ah, 7
+    int 21h
+
+    cmp al, '0'
+    jl near deu_erro
+    cmp al, '9'
+    jg near deu_erro
+ret
 
 fim:
     CLI                    ; Desabilita interrupções
@@ -100,8 +139,11 @@ fim:
     MOV     AX, [offset_dos] ; Carrega AX com o offset anterior de IP
     MOV     [ES:intr*4], AX ; Restaura o offset de IP anterior
     STI                    ; Habilita interrupções
-    MOV     AH, 4Ch        ; Função 4Ch de interrupção 21h: Termina o programa
-    int     21h            ; Chama interrupção 21h para terminar o programa
+    mov ah,0                ; set video mode
+	mov al,[modo_anterior]    ; modo anterior
+	int 10h
+	mov ax, 4C00h
+	int 21h
 
 relogio:
     push    ax             ; Salva o registrador AX na pilha
@@ -109,7 +151,7 @@ relogio:
     mov     ax, data       ; Move o segmento de dados para AX
     mov     ds, ax         ; Define o segmento de dados
     inc     byte [tique]   ; Incrementa o valor de `tique`
-    cmp     byte [tique], 18 ; Compara `tique` com 18 (aproximadamente 1 segundo)
+    cmp     byte [tique], 14 ; Compara `tique` com 18 (aproximadamente 1 segundo)
     jb      Fimrel         ; Se `tique` for menor que 18, pula para Fimrel
     mov     byte [tique], 0 ; Reseta `tique` para 0
     inc     byte [segundo] ; Incrementa `segundo`
@@ -160,12 +202,149 @@ converte:
     MOV     byte [horario], AL ; Armazena o quociente no formato de horário
     ADD     AH, 30h        ; Converte o resto para ASCII
     mov     byte [horario+1], AH ; Armazena o resto no formato de horário
-    mov     ah, 09h        ; Função 09h de interrupção 21h: Imprimir string
-    mov     dx, horario    ; Define a string a ser impressa
-    int     21h            ; Chama interrupção 21h para imprimir a string
+
+    escreve_horario:
+	mov     	cx,8			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,10			;linha 0-29
+	mov     	dl,32			;coluna 0-79
+	loop_horario:
+		call	cursor
+		mov     al,[bx+horario]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+	loop    loop_horario
+
+    ;mov     ah, 09h        ; Função 09h de interrupção 21h: Imprimir string
+    ;mov     dx, horario    ; Define a string a ser impressa
+    ;int     21h            ; Chama interrupção 21h para imprimir a string
     pop     ds             ; Restaura o registrador DS
     pop     ax             ; Restaura o registrador AX
-    ret                    ; Retorna da função
+ret                        ; Retorna da função
+
+deu_erro:
+    mov byte[cor], vermelho
+    call escreve_erro
+    jmp l1
+
+apaga_erro:
+    mov byte[cor], preto
+    call escreve_erro
+    call converte
+    jmp l1
+
+escreve_erro:
+    mov     	cx,25			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,14			;linha 0-29
+	mov     	dl,23			;coluna 0-79
+	loop_erro:
+		call	cursor
+		mov     al,[bx+mensagem_erro]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+	loop    loop_erro
+    mov byte[cor], branco_intenso
+ret
+
+escreve_menu:
+	mov     	cx,35			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,1			;linha 0-29
+	mov     	dl,20			;coluna 0-79
+	loop_titulo:
+		call	cursor
+		mov     al,[bx+titulo]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+	loop    loop_titulo
+
+    mov     	cx,15			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,17			;linha 0-29
+	mov     	dl,28			;coluna 0-79
+	loop_titulo_menu:
+		call	cursor
+		mov     al,[bx+palavra_titulo_menu]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_titulo_menu
+    
+
+    mov     	cx,7			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,19			;linha 0-29
+	mov     	dl,32			;coluna 0-79
+	loop_menu_q:
+		call	cursor
+		mov     al,[bx+palavra_menu_q]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_menu_q
+    
+	mov     	cx,53			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,20			;linha 0-29
+	mov     	dl,10			;coluna 0-79
+	loop_menu_s:
+		call	cursor
+		mov     al,[bx+palavra_menu_s]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_menu_s
+    
+    mov     	cx,52			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,21			;linha 0-29
+	mov     	dl,11			;coluna 0-79
+	loop_menu_m:
+		call	cursor
+		mov     al,[bx+palavra_menu_m]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_menu_m
+    
+    mov     	cx,50			;n�mero de caracteres
+	mov     	bx,0
+	mov     	dh,22			;linha 0-29
+	mov     	dl,11			;coluna 0-79
+	loop_menu_h:
+		call	cursor
+		mov     al,[bx+palavra_menu_h]
+		call	caracter
+		inc     bx			;proximo caracter
+		inc		dl			;avanca a coluna
+		loop    loop_menu_h
+ret
+
+cursor:
+	pushf
+	push 		ax
+	push 		bx
+	push		cx
+	push		dx
+	push		si
+	push		di
+	push		bp
+	mov     	ah,2
+	mov     	bh,0
+	int     	10h
+	pop		bp
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+	popf
+	ret
 
 
 caracter:
@@ -231,6 +410,14 @@ segment data
     amarelo		equ		14
     branco_intenso	equ		15
 
+    titulo db 'TL_2024/1 - GUILHERME RAIBOLT EFGEN'
+    palavra_titulo_menu db 'Menu de teclas:'
+    palavra_menu_q db 'q: sair'
+    palavra_menu_s db 's: para o contador dos segundos e aguarda novo valor.'
+    palavra_menu_m db 'm: para o contador dos minutos e aguarda novo valor.'
+    palavra_menu_h db 'h: para o contador das horas e aguarda novo valor.'
+    mensagem_erro db 'Valor inserido invalido!!'  ;25
+    
 
     modo_anterior	db		0
     eoi         EQU 20h    ; Define o valor EOI (End of Interrupt)
